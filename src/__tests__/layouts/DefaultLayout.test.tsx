a1b8cabc19dd151db174e2b20e4906aa13035163
Bi Wu <alerk_star@outlook.com>
2022-05-24 22:17:58 +0800

    unit test for DefaultLayout

---
+++ src/__tests__/layouts/DefaultLayout.test.tsx
@@ -0,0 +1,21 @@
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom";
+import { DefaultLayout } from 'layouts/DefaultLayout';
+
+describe('DefaultLayout', () => {
+  test('should have header title', () => {
+    render(<DefaultLayout />, { wrapper: MemoryRouter });
+    const titleElement = screen.getByText(/react/i);
+    expect(titleElement).toBeInTheDocument();
+  })
+
+  test('should render the child component', () => {
+    render(<DefaultLayout>
+      <h3>Demo Content</h3>
+    </DefaultLayout>, { wrapper: MemoryRouter });
+
+    const childElement = screen.getByText(/demo content/i);
+    expect(childElement).toBeInTheDocument();
+  })
+})
